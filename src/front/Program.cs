b733339a5651da64ef3620ce03762f510e48827c
Rexy Que <rexy.que@gmail.com>
2019-11-02 17:04:00 -0500

    Initiate project files

---
+++ src/front/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace DevIct.PublicMeetings.Data.Front
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
